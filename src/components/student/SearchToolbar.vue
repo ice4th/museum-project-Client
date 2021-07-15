394ad218e8c86098c0ea273a8576dec3c3627b80
Dusadee <dusadee.srj@gmail.com>
2021-07-15 20:15:03 +0700

    create dynamic table media

---
+++ src/components/student/SearchToolbar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex-table-wrapper mt-4">
+    <div class="flex-table-toolbar">
+      <div class="left">
+        <V-Field>
+          <V-Control icon="feather:search">
+            <input
+              type="text"
+              class="input is-rounded"
+              placeholder="Search..."
+            />
+          </V-Control>
+        </V-Field>
+      </div>
+
+      <div class="right">
+        <V-Field>
+          <V-Control>
+            <div class="select is-rounded">
+              <select>
+                <option selected>10 results per page</option>
+                <option>25 results per page</option>
+                <option>50 results per page</option>
+                <option>100 results per page</option>
+              </select>
+            </div>
+          </V-Control>
+        </V-Field>
+      </div>
+    </div>
+  </div>
+</template>
