c0211af3372ebc931d8b68296a4f01b5d61a51f5
phulinMN <pongpaiMN@gmail.com>
2021-08-16 19:59:20 +0700

    add permission and role side bar and action

--- src/helpers/permission/main-sidebar.ts
+++ src/helpers/permission/main-sidebar.ts
@@ -2,7 +2,12 @@ import { NavbarItemDetail } from './sidebar.interface'
 
 export const mainSidebar: NavbarItemDetail[] = [
   { key: 'admin', label: 'Admin', icon: 'feather:users' },
+  {
+    key: 'permission',
+    label: 'Roles & Permissions',
+    icon: 'feather:unlock',
+  },
   { key: 'student', label: 'Students', icon: 'feather:users' },
   { key: 'product', label: 'Products & Packages', icon: 'feather:box' },
-  { key: 'code', label: 'Code Management', icon: 'feather:gift ' },
+  { key: 'code', label: 'Code', icon: 'feather:gift' },
 ]
