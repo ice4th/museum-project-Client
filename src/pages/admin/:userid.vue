68ec5f250c8c7d500058428a140a0be0621473be
ice4th <patteerathisri@gmail.com>
2021-08-19 16:15:13 +0700

    admin frofile

--- src/pages/admin/:userid.vue
+++ src/pages/admin/:userid.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head'
 import { useWindowScroll } from '@vueuse/core'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import useViewAdmin from '/@src/composable/admin/use-view-admin'
+
+const options = ref([])
+const country = ref('th')
+
+const userInfo = ref({
+  firstname: 'aaa',
+  lastname: 'ccc',
+  name: 'ice',
+  phone: '9989',
+  email: 'dddddd',
+  team: 'engineer',
+  role: 'superAdmin',
+  country: 'th',
+  manageCountry: [],
+})
 
 const { y } = useWindowScroll()
 const isStuck = computed(() => {
@@ -43,7 +58,12 @@ const isStuck = computed(() => {
                 <V-Field>
                   <label>Firstname</label>
                   <V-Control>
-                    <input type="text" class="input" placeholder="Firstname" />
+                    <input
+                      v-model="userInfo.firstname"
+                      type="text"
+                      class="input"
+                      placeholder="Firstname"
+                    />
                     <p class="help">Firstname</p>
                   </V-Control>
                 </V-Field>
@@ -52,7 +72,12 @@ const isStuck = computed(() => {
                 <V-Field>
                   <label>Lastname</label>
                   <V-Control>
-                    <input type="text" class="input" placeholder="Firstname" />
+                    <input
+                      v-model="userInfo.lastname"
+                      type="text"
+                      class="input"
+                      placeholder="Firstname"
+                    />
                     <p class="help">Lastname</p>
                   </V-Control>
                 </V-Field>
@@ -61,7 +86,12 @@ const isStuck = computed(() => {
                 <V-Field>
                   <label>Name</label>
                   <V-Control>
-                    <input type="text" class="input" placeholder="Firstname" />
+                    <input
+                      v-model="userInfo.name"
+                      type="text"
+                      class="input"
+                      placeholder="Firstname"
+                    />
                   </V-Control>
                 </V-Field>
               </div>
@@ -70,6 +100,7 @@ const isStuck = computed(() => {
                   <label>Phone</label>
                   <V-Control>
                     <input
+                      v-model="userInfo.phone"
                       type="text"
                       class="input"
                       placeholder="Phone Number"
@@ -82,9 +113,10 @@ const isStuck = computed(() => {
                   <label>Email</label>
                   <V-Control>
                     <input
+                      v-model="userInfo.email"
                       type="text"
                       class="input"
-                      placeholder="abc@globish.co.th"
+                      readonly
                     />
                   </V-Control>
                 </V-Field>
@@ -93,7 +125,12 @@ const isStuck = computed(() => {
                 <V-Field>
                   <label>Team</label>
                   <V-Control>
-                    <Multiselect placeholder="Select your team"> </Multiselect>
+                    <input
+                      v-model="userInfo.team"
+                      type="text"
+                      class="input"
+                      readonly
+                    />
                   </V-Control>
                 </V-Field>
               </div>
@@ -101,7 +138,12 @@ const isStuck = computed(() => {
                 <V-Field>
                   <label>Role</label>
                   <V-Control>
-                    <Multiselect placeholder="Select your role"> </Multiselect>
+                    <input
+                      v-model="userInfo.role"
+                      type="text"
+                      class="input"
+                      readonly
+                    />
                   </V-Control>
                 </V-Field>
               </div>
@@ -109,16 +151,32 @@ const isStuck = computed(() => {
                 <V-Field>
                   <label>Country</label>
                   <V-Control>
-                    <Multiselect placeholder="Select your country">
+                    <Multiselect
+                      v-model="userInfo.country"
+                      :options="['th', 'vn']"
+                      placeholder="Select your country"
+                    >
                     </Multiselect>
                   </V-Control>
                 </V-Field>
               </div>
-              <div class="column">
+              <div class="column is-12">
                 <V-Field>
-                  <label>Status</label>
+                  <!-- check box -->
+                  <label>Manage Country</label>
                   <V-Control>
-                    <V-SwitchBlock color="primary" checked />
+                    <V-Checkbox
+                      v-model="userInfo.manageCountry"
+                      value="th"
+                      label="th"
+                      color="primary"
+                    />
+                    <V-Checkbox
+                      v-model="userInfo.manageCountry"
+                      value="vn"
+                      label="vn"
+                      color="primary"
+                    />
                   </V-Control>
                 </V-Field>
               </div>
