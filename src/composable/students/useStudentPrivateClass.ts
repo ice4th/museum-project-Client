9d68b672fb7473c303fd493e29cb77f88d33f3b6
phulinMN <pongpaiMN@gmail.com>
2021-09-17 23:07:16 +0700

    change use pagination route in private class

--- src/composable/students/useStudentPrivateClass.ts
+++ src/composable/students/useStudentPrivateClass.ts
@@ -1,7 +1,7 @@
 import { onMounted, reactive, toRefs } from 'vue'
 import { useRoute } from 'vue-router'
 import useStudentApi from '/@src/composable/api/useStudentApi'
-import usePaginationRoute from '/@src/composable/use-pagination-route'
+import usePaginationRoute from '/@src/composable/usePaginationRoute'
 import { IStudentPrivateClass } from '/@src/types/interfaces/student.interface'
 interface UseStudentListState {
   data: IStudentPrivateClass[]
